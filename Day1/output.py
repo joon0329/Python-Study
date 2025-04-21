59f25e42681f3105ff5dfbdbe511cc3627b06858
장혁준 <janghyeogjun@janghyeogjun-ui-MacBookAir.local>
2025-04-21 22:04:47 +0900

    First day: Study @local

---
+++ Day1/output.py
@@ -0,0 +1,14 @@
+print("#하나만 출력")
+print("Hello Python Programming...!")
+print() #줄바꿈
+
+print("#여러개 출력")
+print(10, 20, 30, 40, 50)
+print("안녕하세요", "저의", "이름은", "윤인성입니다!")
+print()
+
+print("#아무것도 출력하지 않습니다.")
+print("--확인전용선--")
+print()
+print()
+print("--확인전용선--")
